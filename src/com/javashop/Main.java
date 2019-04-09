c5c6ff3f6379d54e6c2afb447daed8978c29b294
Marius <Marius@DESKTOP-U12H2LU>
2019-04-09 18:36:21 +0300

    add project

---
+++ src/com/javashop/Main.java
@@ -0,0 +1,25 @@
+package com.javashop;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+/*
+        try {
+            //1. get connection to database
+            Connection myConn = DriverManager.getConnection("jdbc:mysql://db4free.net:3306/shopdb2019", "proiectjava2019", "proiectjava2019");
+            //2. create a statement
+            Statement myStatement = myConn.createStatement();
+            //3. execute sql query
+            ResultSet myResultSet = myStatement.executeQuery("select * from products");
+            //4. process the result set
+            while (myResultSet.next()) {
+                System.out.println(myResultSet.getString("Pret"));
+            }
+            //5. insert a row in table
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+    }
+}
